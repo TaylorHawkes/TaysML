9555a0f1653f330031e2625273c6fa4d9dab45ac
taylor hawkes <taylorhawkes@taylors-MacBook-Pro.local>
2022-02-11 10:32:27 -0700

    network change

--- Network.php
+++ Network.php
@@ -162,11 +162,9 @@ class Network
     }
     //end grepper
     
-    //grepper php sigmoid function
     public function sigmoid($t){
         return 1 / (1 + exp(-$t));
     }
-    //end grepper
 
     public function matrix_sigmoid($t){
         return 1.0 / (1.0 + \CArray::exp(-$t));
