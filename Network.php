1eae6b2fdb475f399e3133e7db4f947cac5f0e4f
taylor hawkes <taylorhawkes@taylors-MacBook-Pro.local>
2022-02-11 11:37:10 -0700

    test

--- Network.php
+++ Network.php
@@ -163,7 +163,7 @@ class Network
     //end grepper
     
 
-    //grepper php this is a sigmoid function 
+    //grepper test php this is a sigmoid function 
     public function sigmoid($t){
         return 1 / (1 + exp(-$t));
     }
